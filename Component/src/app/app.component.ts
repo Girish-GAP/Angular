9f985fed3396ed39e58cefaede4fff2461528012
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-15 09:47:40 +0530

    Child comp. creation and nested comp. rendering

---
+++ Component/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Application5';
+
+  msg = 'Msg from app component'
+}
---
+++ Component/src/app/second-component/second-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  msg = 'msg from Second component'
+
+}
