62c103587ddeff6a27a1d4239de3cf5243c6e5b9
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-15 08:52:22 +0530

    Create a new Component

---
+++ src/app/demo-component/demo-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-component',
+  templateUrl: './demo-component.component.html',
+  styleUrls: ['./demo-component.component.css']
+})
+export class DemoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title = 'From demo.ts'
+
+}
