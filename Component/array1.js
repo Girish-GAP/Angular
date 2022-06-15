9dd548db7f1553abc5d02f32450f2fbbf34a73ab
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-15 07:28:34 +0530

    Add files via upload

---
+++ Component/array1.js
@@ -0,0 +1,9 @@
+// int arr[5] = {10,20,30}; // C /CPP /JAVA / C#
+// Arr is one dimentional array which contains 5 elements each elements is of type number.
+var Arr = [10, 20, 30, 40, 50];
+console.log("Length of array is : " + Arr.length);
+console.log("First element is : " + Arr[0]);
+console.log("Second element is : " + Arr[1]);
+for (var i = 0; i < Arr.length; i++) {
+    console.log("\n" + Arr[i]);
+}
