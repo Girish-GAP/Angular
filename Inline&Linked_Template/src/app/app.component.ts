19756d382ccfdbd18b973d1d2856637718769a3e
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-16 07:33:51 +0530

    Update app.component.ts

--- Inline&Linked_Template/src/app/app.component.ts
+++ Inline&Linked_Template/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',    // To Linked template we gave url of HTML file
+  styleUrls: ['./app.component.css']      // To Linked CSS we gave url of CSS file
 })
 
 
