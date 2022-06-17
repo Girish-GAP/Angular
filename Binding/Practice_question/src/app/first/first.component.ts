90f0c32219b77a1ec70996072e4bc4f2728e040b
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-17 15:25:55 +0530

    Practice question of binding

---
+++ Binding/Practice_question/src/app/first/first.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Function return string
+  call()
+  {
+    return "Marvellous Infosystems";
+  }
+
+}
