421357411e6a86b4498420edfe23f8eb9324b40b
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-17 18:43:57 +0530

    Two way binding

---
+++ Binding/Two way binding/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
+
+// To import ngModel which help to achive two-way-data binding.
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TwowaybindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
