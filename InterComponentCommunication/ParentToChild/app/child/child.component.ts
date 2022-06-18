c7d723aabc02a211094ce8d9d1952834995fe398
Girish <82331285+Girish-GAP@users.noreply.github.com>
2022-06-18 17:57:41 +0530

    Update child.component.ts

--- InterComponentCommunication/ParentToChild/app/child/child.component.ts
+++ InterComponentCommunication/ParentToChild/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input} from '@angular/core';    // We have to import input from '@angular/core'
 
 @Component({
   selector: 'app-child',
